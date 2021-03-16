4b1e0cfdd248a63410613539fa203fdca7a8639f
Keyan Pishdadian <kpishdadian@gmail.com>
2021-03-16 14:28:08 -0700

    Initial commit, contents from prompt

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "network.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        std::cout << "Error: requires initial and final supercharger names" << std::endl;        
+        return -1;
+    }
+    
+    std::string initial_charger_name = argv[1];
+    std::string goal_charger_name = argv[2];
+
+    return 0;
+}
---
+++ network.h
@@ -0,0 +1,20 @@
+/*************************************************/
+/********* DO NOT MODIFY THIS FILE ************/
+/*************************************************/
+#pragma once
+#include <string>
+#include <iostream>
+#include <array>
+
+struct row
+{
+    std::string name;
+    double lat;
+    double lon;
+    double rate;
+};
+
+extern std::array<row, 303> network;
+/*************************************************/
+/********* DO NOT MODIFY THIS FILE ************/
+/*************************************************/
